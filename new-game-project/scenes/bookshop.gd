e231c9d15746bcd62bd081b30d43c8a3af0c3ee0
MartinH4364 <martinho4364@gmail.com>
2025-09-28 10:16:42 -0700

    Started making shop

--- new-game-project/scenes/bookshop.gd
+++ new-game-project/scenes/bookshop.gd
@@ -11,3 +11,8 @@ func _on_body_exited(body):
 	if body.name == "ProtoController":
 		player_in_range = false
 		$"../Sketchfab_Scene/SubViewport/Label".text = " "  # hide the label
+		
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("interact") and player_in_range:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		get_tree().change_scene_to_file("res://scenes/Shop.tscn")
