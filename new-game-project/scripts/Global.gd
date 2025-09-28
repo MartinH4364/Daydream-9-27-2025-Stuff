702019faf1009ced61b994cc143603be6b0401dd
MartinH4364 <martinho4364@gmail.com>
2025-09-28 14:19:41 -0700

    added more coins

--- new-game-project/scripts/Global.gd
+++ new-game-project/scripts/Global.gd
@@ -7,14 +7,14 @@ var maxSanity = 360
 var sanity = 400
 var sanityLoss = 1
 var FogAdd = 0.7
-var coffeeAmount = 4
+var coffeeAmount = 0
 var mushroomAmount = 0
 var coinMultiplier = 1
 
 func _process(delta: float) -> void:
 	if sanity < 0:
 		get_tree().change_scene_to_file("res://scenes/Youlose.tscn")
-	FogAdd = 0.7+coffeeAmount * 0.025
+	FogAdd = 0.7+coffeeAmount * 0.05
 	timerTime = 40+20*coffeeAmount
 	coinMultiplier = 1+mushroomAmount
 	sanityLoss = 1 - 0.05*mushroomAmount
