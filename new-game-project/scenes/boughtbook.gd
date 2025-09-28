b1bba7a1b6b8087ee1026ca9e1c7598f977150fa
MartinH4364 <martinho4364@gmail.com>
2025-09-28 13:44:04 -0700

    balanced game

--- new-game-project/scenes/boughtbook.gd
+++ new-game-project/scenes/boughtbook.gd
@@ -1,6 +1,6 @@
 extends Button
 
 func _on_pressed() -> void:
-	if tickets >= 50:
-		Global.tickets -= 50
-		Global.sanity += 100
+	if Global.tickets >= 30:
+		Global.tickets -= 30
+		Global.sanity += 50
