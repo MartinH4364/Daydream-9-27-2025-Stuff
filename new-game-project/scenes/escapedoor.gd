cb812a4c92b055362aa145719b57a738ab6d79ea
surbletm <enmingz936@gmail.com>
2025-09-28 14:46:30 -0700

    feat: secret ending

--- new-game-project/scenes/escapedoor.gd
+++ new-game-project/scenes/escapedoor.gd
@@ -1,8 +1,17 @@
-extends Area3D
+var player_in_range = false
+@onready var interact_label: Label = %spedmonkey
 
 
-func _on_body_entered(_body):
-	$"../SubViewport/Label".text = "press E to ESCAPE"   # show the label
+func _on_body_entered(body):
+	if body.name == "ProtoController":
+		player_in_range = true
+		interact_label.text = "press E to ESCAPE"   # show the label
 
-func _on_body_exited(_body):
-	$"../SubViewport/Label".text = " "  # hide the label
+func _on_body_exited(body):
+	if body.name == "ProtoController":
+		player_in_range = false
+		interact_label.text = " "  # hide the label
+
+func _process(delta: float) -> void:
+	if player_in_range and Input.is_action_just_pressed("interact"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
